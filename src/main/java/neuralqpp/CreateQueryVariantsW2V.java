2809f60a2e8df0e61ed48976930dc76bad31b679
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-05-11 19:57:12 +0100

    dealing with null results of NN uplook - debug 1

--- src/main/java/neuralqpp/CreateQueryVariantsW2V.java
+++ src/main/java/neuralqpp/CreateQueryVariantsW2V.java
@@ -129,6 +129,7 @@ public class CreateQueryVariantsW2V {
                     choice = rand.nextInt(NNList.length);
                     System.out.println("roulette : " + choice);
                     selectTerm = NNList[choice];
+                    System.out.println("selected substituion : " + selectTerm);
                     if (!qtermChoice.contains(selectTerm)) {
                         qtermChoice = qtermChoice + " " + selectTerm;
                         System.out.println("new query : " + qtermChoice + "\n");
